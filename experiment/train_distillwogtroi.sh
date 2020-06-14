33db09fd254abec090897cca43ba0b96f91713f1
HaoIrving <sunwenhao2018@ia.ac.cn>
2020-06-14 20:55:04 +0800

    lr 001

--- experiment/train_distillwogtroi.sh
+++ experiment/train_distillwogtroi.sh
@@ -6,4 +6,4 @@ CUDA_VISIBLE_DEVICES=0 python tools/train_distillwogtroi.py --num-gpus 1 \
         --path_t checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1/model_final.pth \
         --kd_T 4 \
         OUTPUT_DIR "checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_distillwogtroi_g1_t4_lr002" \
-        SOLVER.IMS_PER_BATCH "16" SOLVER.BASE_LR "0.002" 
+        SOLVER.IMS_PER_BATCH "16" SOLVER.BASE_LR "0.001" 
