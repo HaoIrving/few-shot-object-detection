50c48055ccaba6d555efac728acb0b4f2874ab78
SUN Wenhao <49302818+HaoIrving@users.noreply.github.com>
2020-06-18 11:02:51 +0800

    fix dist-url

--- experiment/test_dt_w_gtroi_roi.sh
+++ experiment/test_dt_w_gtroi_roi.sh
@@ -2,4 +2,4 @@ cd ..
 
 CUDA_VISIBLE_DEVICES=2,3 python tools/test_net.py --num-gpus 2 \
         --config-file checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_roi_g1_t4_lr003/config.yaml \
-        --eval-all --dist_url "auto"
+        --eval-all --dist-url "auto"
