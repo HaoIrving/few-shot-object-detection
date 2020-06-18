945692f25da8fb989f2ac84b96607721dfa5d8fd
HaoIrving <sunwenhao2018@ia.ac.cn>
2020-06-18 16:31:59 +0800

    --start-iter 9999

--- experiment/test_dt_w_gtroi_roi.sh
+++ experiment/test_dt_w_gtroi_roi.sh
@@ -2,4 +2,4 @@ cd ..
 
 CUDA_VISIBLE_DEVICES=2,3 python tools/test_net.py --num-gpus 2 \
         --config-file checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_roi_g1_t4_lr003/config.yaml \
-        --eval-all --dist-url "auto"
+        --eval-all --dist-url "auto" --start-iter 9999
