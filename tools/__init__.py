f094541a8bc10655df18dfd4e1e3fd67857857ac
SUN Wenhao <49302818+HaoIrving@users.noreply.github.com>
2020-06-18 13:06:46 +0800

    register GeneralizedRCNN_distill, RPN_distill, StandardROIHeads_distill

---
+++ tools/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+# from .build import META_ARCH_REGISTRY, build_model  # isort:skip
+
+# import all the meta_arch, so they will be registered
+from train_dt_w_gtroi_roi import GeneralizedRCNN_distill, RPN_distill, StandardROIHeads_distill
