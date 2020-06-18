a04362497908c3e2f3657530f00d13a6f562146a
HaoIrving <sunwenhao2018@ia.ac.cn>
2020-06-18 20:33:31 +0800

    delete last fir

--- experiment/train_dt_w_gtroi_rpn_roi.sh
+++ experiment/train_dt_w_gtroi_rpn_roi.sh
@@ -1,7 +1,7 @@
 cd ..
 
 # TODO: check every time
-# rm -rf checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn+roi_g1_t4_lr005
+rm -rf checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_roi_g1_t4_lr003_3e4
 
 # g1_t4_lr002
 CUDA_VISIBLE_DEVICES=0,1 python tools/train_dt_w_gtroi_rpn_roi.py --num-gpus 2 \
