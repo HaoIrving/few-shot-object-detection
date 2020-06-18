92790deec130649289ebf6d4a980658ff95206c0
HaoIrving <sunwenhao2018@ia.ac.cn>
2020-06-18 19:56:02 +0800

    max iter 3e4

--- experiment/test_dt_w_gtroi_roi.sh
+++ experiment/test_dt_w_gtroi_roi.sh
@@ -1,5 +1,5 @@
 cd ..
 
-CUDA_VISIBLE_DEVICES=2,3 python tools/test_net.py --num-gpus 2 \
-        --config-file checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_roi_g1_t4_lr003/config.yaml \
-        --eval-all --dist-url "auto" --start-iter 9999
+CUDA_VISIBLE_DEVICES=2 python tools/test_net.py --num-gpus 1 \
+        --config-file checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_roi_g1_t4_lr003_3e4/config.yaml \
+        --eval-all --dist-url "auto" --start-iter 14999
--- experiment/train_dt_w_gtroi_roi.sh
+++ experiment/train_dt_w_gtroi_roi.sh
@@ -1,15 +1,18 @@
 cd ..
 
 # TODO: check every time
-rm -rf checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_roi_g1_t4_lr005
+# rm -rf checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_roi_g1_t4_lr005
 
 # g1_t4_lr002
 CUDA_VISIBLE_DEVICES=2,3 python tools/train_dt_w_gtroi_roi.py --num-gpus 2 \
         --config-file configs/PascalVOC-detection/split1/faster_rcnn_R_101_FPN_base1.yaml \
         --path_t checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1/model_final.pth \
         --kd_T 4 --dist-url "auto"\
-        OUTPUT_DIR "checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_roi_g1_t4_lr003" \
-        SOLVER.IMS_PER_BATCH "16" SOLVER.BASE_LR "0.003" SOLVER.CHECKPOINT_PERIOD "1000"
+        OUTPUT_DIR "checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_roi_g1_t4_lr003_3e4" \
+        SOLVER.IMS_PER_BATCH "16" SOLVER.BASE_LR "0.003" SOLVER.CHECKPOINT_PERIOD "1000"\
+        SOLVER.MAX_ITER "30000"
 
-rm -rf ~/Tensorboard
-cp -r ./checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_roi_g1_t4_lr003/ ~/Tensorboard
+# rm -rf ~/Tensorboard
+# cp -r ./checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_roi_g1_t4_lr003/ ~/Tensorboard
+
+bash experiment/test_dt_w_gtroi_roi.sh
