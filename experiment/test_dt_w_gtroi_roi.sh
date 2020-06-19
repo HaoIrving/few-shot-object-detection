cdd2dc014323fada6172f2b96ecd04ab1678fcb9
SUN Wenhao <49302818+HaoIrving@users.noreply.github.com>
2020-06-19 22:10:24 +0800

    max iter 5e4 to dig out the potentials of two models

--- experiment/test_dt_w_gtroi_roi.sh
+++ experiment/test_dt_w_gtroi_roi.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=2 python tools/test_net.py --num-gpus 1 \
-        --config-file checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_roi_g1_t4_lr003_3e4/config.yaml \
-        --eval-all --dist-url "auto" --start-iter 14999
+        --config-file checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_roi_g1_t4_lr003_5e4/config.yaml \
+        --eval-all --dist-url "auto" --start-iter 25999
--- experiment/train_dt_w_gtroi_roi.sh
+++ experiment/train_dt_w_gtroi_roi.sh
@@ -1,16 +1,16 @@
 cd ..
 
 # TODO: check every time
-rm -rf checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_roi_g1_t4_lr003_3e4
+# rm -rf checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_roi_g1_t4_lr003_3e4
 
 # g1_t4_lr002
 CUDA_VISIBLE_DEVICES=2,3 python tools/train_dt_w_gtroi_roi.py --num-gpus 2 \
         --config-file configs/PascalVOC-detection/split1/faster_rcnn_R_101_FPN_base1.yaml \
         --path_t checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1/model_final.pth \
         --kd_T 4 --dist-url "auto"\
-        OUTPUT_DIR "checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_roi_g1_t4_lr003_3e4" \
+        OUTPUT_DIR "checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_roi_g1_t4_lr003_5e4" \
         SOLVER.IMS_PER_BATCH "16" SOLVER.BASE_LR "0.003" SOLVER.CHECKPOINT_PERIOD "1000"\
-        SOLVER.MAX_ITER "30000" SOLVER.STEPS "(20000, 26700)" SOLVER.WARMUP_ITERS "200"
+        SOLVER.MAX_ITER "50000" SOLVER.STEPS "(33334, 44445)" SOLVER.WARMUP_ITERS "278"
 
 # rm -rf ~/Tensorboard
 # cp -r ./checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_roi_g1_t4_lr003/ ~/Tensorboard
