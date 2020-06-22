18b350459272a7de5b745aea2dffbcdb5d6777ca
SUN Wenhao <49302818+HaoIrving@users.noreply.github.com>
2020-06-22 22:17:30 +0800

    1/100 for mse

--- tools/train_dt_w_gtroi_rpn_roi.py
+++ tools/train_dt_w_gtroi_rpn_roi.py
@@ -448,7 +448,7 @@ class Trainer(DefaultTrainer):
         criterion_kd_roi_heads = self.criterion_kd_roi_heads(pred_class_logits_gt_s, pred_class_logits_gt_t)
         loss_distill = {
             "loss_distill_rpn": criterion_kd_rpn, 
-            "loss_distill_roiheads": criterion_kd_roi_heads
+            "loss_distill_roiheads": criterion_kd_roi_heads / 100
             }
 
         loss_dict = {}
