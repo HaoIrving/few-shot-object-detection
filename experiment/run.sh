8f654b58536f675ad25abe0a571ae5e933cfd31f
SUN Wenhao <49302818+HaoIrving@users.noreply.github.com>
2020-06-22 22:22:52 +0800

    pipline

--- experiment/run.sh
+++ experiment/run.sh
@@ -29,10 +29,10 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/train_dt_w_gtroi_rpn_roi.py --num-gpus
         SOLVER.IMS_PER_BATCH "16" SOLVER.BASE_LR "0.003" SOLVER.CHECKPOINT_PERIOD "1000"\
         SOLVER.MAX_ITER "50000" SOLVER.STEPS "(33334, 44445)" SOLVER.WARMUP_ITERS "278"
 
-# rm -rf ~/Tensorboard
-# cp -r ./checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_roi_mse_g1_t4_lr003_5e4/ ~/Tensorboard
+rm -rf ~/Tensorboard
+cp -r ./checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_roi_mse_g1_t4_lr003_5e4/ ~/Tensorboard
 
-# # test to check overfitting
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --num-gpus 4 \
-#         --config-file checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_roi_mse_g1_t4_lr003_5e4/config.yaml \
-#         --eval-all --start-iter 35999
+# test to check overfitting
+CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --num-gpus 4 \
+        --config-file checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_roi_mse_g1_t4_lr003_5e4/config.yaml \
+        --eval-all --start-iter 35999
