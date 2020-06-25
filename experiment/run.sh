e2e6d34c4e7c68bd7d820b8855f353bb759d73c7
SUN Wenhao <49302818+HaoIrving@users.noreply.github.com>
2020-06-25 22:54:01 +0800

    debug

--- experiment/run.sh
+++ experiment/run.sh
@@ -20,21 +20,21 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --num-gpus 4 \
         --config-file checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_mse_roi_t4_g1_lr02_5e4/config.yaml \
         --eval-all --start-iter 33999
 
-rm -rf ~/Tensorboard
+# rm -rf ~/Tensorboard
 
-# train, rpn_roi, kl_kl
-CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/train_dt_w_gtroi_rpn_roi.py --num-gpus 4 \
-        --config-file configs/PascalVOC-detection/split1/faster_rcnn_R_101_FPN_base1.yaml \
-        --path_t checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1/model_final.pth \
-        --kd_T 4 \
-        OUTPUT_DIR "checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_t4_roi_t4_g1_lr02_5e4" \
-        SOLVER.IMS_PER_BATCH "16" SOLVER.BASE_LR "0.02" SOLVER.CHECKPOINT_PERIOD "1000"\
-        SOLVER.MAX_ITER "50000" SOLVER.STEPS "(33334, 44445)" SOLVER.WARMUP_ITERS "278"
+# # train, rpn_roi, kl_kl
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/train_dt_w_gtroi_rpn_roi.py --num-gpus 4 \
+#         --config-file configs/PascalVOC-detection/split1/faster_rcnn_R_101_FPN_base1.yaml \
+#         --path_t checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1/model_final.pth \
+#         --kd_T 4 \
+#         OUTPUT_DIR "checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_t4_roi_t4_g1_lr02_5e4" \
+#         SOLVER.IMS_PER_BATCH "16" SOLVER.BASE_LR "0.02" SOLVER.CHECKPOINT_PERIOD "1000"\
+#         SOLVER.MAX_ITER "50000" SOLVER.STEPS "(33334, 44445)" SOLVER.WARMUP_ITERS "278"
 
-rm -rf ~/Tensorboard
-cp -r ./checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_t4_roi_t4_g1_lr02_5e4/ ~/Tensorboard
+# rm -rf ~/Tensorboard
+# cp -r ./checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_t4_roi_t4_g1_lr02_5e4/ ~/Tensorboard
 
-# test to check overfitting
-CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --num-gpus 4 \
-        --config-file checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_t4_roi_t4_g1_lr02_5e4/config.yaml \
-        --eval-all --start-iter 20999
+# # test to check overfitting
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --num-gpus 4 \
+#         --config-file checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_t4_roi_t4_g1_lr02_5e4/config.yaml \
+#         --eval-all --start-iter 20999
