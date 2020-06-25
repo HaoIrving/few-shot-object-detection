eed22bb54ae68b3b8c70d7a6780779ef07e77b4e
SUN Wenhao <49302818+HaoIrving@users.noreply.github.com>
2020-06-25 22:51:07 +0800

    debug

--- experiment/run.sh
+++ experiment/run.sh
@@ -20,6 +20,8 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --num-gpus 4 \
         --config-file checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_mse_roi_t4_g1_lr02_5e4/config.yaml \
         --eval-all --start-iter 33999
 
+rm -rf ~/Tensorboard
+
 # train, rpn_roi, kl_kl
 CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/train_dt_w_gtroi_rpn_roi.py --num-gpus 4 \
         --config-file configs/PascalVOC-detection/split1/faster_rcnn_R_101_FPN_base1.yaml \
