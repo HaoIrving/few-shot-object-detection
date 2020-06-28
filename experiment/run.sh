b5e6490adc656c8da5885bc2cc63f3c6dbd1b04f
SUN Wenhao <49302818+HaoIrving@users.noreply.github.com>
2020-06-28 09:53:47 +0800

    debug

--- experiment/run.sh
+++ experiment/run.sh
@@ -7,7 +7,7 @@ cd ..
 CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/train_dt_w_gtroi_rpn_roi.py --num-gpus 4 \
         --config-file configs/PascalVOC-detection/split1/faster_rcnn_R_101_FPN_base1.yaml \
         --path_t checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1/model_final.pth \
-        --kd_T 4 --resume\
+        --kd_T 4 \
         OUTPUT_DIR "checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_t4_roi_t4_g1_lr02_2e4" \
         SOLVER.IMS_PER_BATCH "16" SOLVER.BASE_LR "0.02" SOLVER.CHECKPOINT_PERIOD "1000"\
         SOLVER.MAX_ITER "25000" \
