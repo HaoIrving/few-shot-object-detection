9ce5d09d86d90cfe8769b5adc66bcce50d3292fd
SUN Wenhao <49302818+HaoIrving@users.noreply.github.com>
2020-06-30 09:05:19 +0800

    rpn_100mse_roi_100mse_g1_lr02_2e4

--- tools/train_dt_w_gtroi_rpn_roi.py
+++ tools/train_dt_w_gtroi_rpn_roi.py
@@ -434,8 +434,8 @@ class Trainer(DefaultTrainer):
 
         # self.criterion_kd_rpn = HintLoss()
         self.criterion_kd_rpn = DistillKL(kd_T)
-        self.criterion_kd_roi_heads = DistillKL(kd_T)# TODO: grid search, LWF
-        # self.criterion_kd_roi_heads = HintLoss()
+        # self.criterion_kd_roi_heads = DistillKL(kd_T)# TODO: grid search, LWF
+        self.criterion_kd_roi_heads = HintLoss()
         if torch.cuda.is_available():
             self.criterion_kd_rpn.cuda()
             self.criterion_kd_roi_heads.cuda()
