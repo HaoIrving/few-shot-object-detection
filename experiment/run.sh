4927137e31c417501b4f801f0b9afd5061d29b5d
SUN Wenhao <49302818+HaoIrving@users.noreply.github.com>
2020-07-01 11:12:23 +0800

    rpn_100mse_roi_100t4_g3_lr02_2e4

--- experiment/run.sh
+++ experiment/run.sh
@@ -22,17 +22,17 @@ cd ..
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/train_dt_w_gtroi_rpn_roi.py --num-gpus 4 \
         --config-file configs/PascalVOC-detection/split1/faster_rcnn_R_101_FPN_base1.yaml \
-        --path_t checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_100mse_roi_100t4_g1_lr02_2e4/model_0023999.pth \
+        --path_t checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_100mse_roi_100t4_g2_lr02_2e4/model_0023999.pth \
         --kd_T 4 \
-        OUTPUT_DIR "checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_100mse_roi_100t4_g2_lr02_2e4" \
+        OUTPUT_DIR "checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_100mse_roi_100t4_g3_lr02_2e4" \
         SOLVER.IMS_PER_BATCH "16" SOLVER.BASE_LR "0.02" SOLVER.CHECKPOINT_PERIOD "1000"\
         SOLVER.MAX_ITER "25000" \
         # SOLVER.STEPS "(33334, 44445)" SOLVER.WARMUP_ITERS "278"
 
-rm -rf ~/Tensorboard
-cp -r ./checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_100mse_roi_100t4_g2_lr02_2e4/ ~/Tensorboard
+# rm -rf ~/Tensorboard
+# cp -r ./checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_100mse_roi_100t4_g2_lr02_2e4/ ~/Tensorboard
 
 # test to check overfitting
 CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/test_net.py --num-gpus 4 \
-        --config-file checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_100mse_roi_100t4_g2_lr02_2e4/config.yaml \
+        --config-file checkpoints/voc/faster_rcnn/faster_rcnn_R_101_FPN_base1_dt_w_gtroi_rpn_100mse_roi_100t4_g3_lr02_2e4/config.yaml \
         --eval-all --start-iter 14999
